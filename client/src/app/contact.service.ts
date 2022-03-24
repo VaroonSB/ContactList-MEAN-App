34cd674a86c42627f1cf3d87a33c4a4464d81ab7
VaroonSB <varoonsb1311@gmail.com>
2022-03-24 14:26:08 +0530

    contacts schema in angular

--- client/src/app/contact.service.ts
+++ client/src/app/contact.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+// import { contact } from "./contact";
+import { Contact } from './contact';
+import {HttpClient} from "@angular/common/http";
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getContacts(){
+    return this.http.get("http://localhost:3000/api/contacts").pipe(map(res => res));
+  }
+
+  addContact(newContact:string){
+    // const header = new Headers();
+    // headers.append('Content-Type', 'application/json');
+    return this.http.post("http://localhost:3000/api/contacts", newContact).pipe(map(res => res));
+  }
+
+  deleteContact(id:string){
+    return this.http.delete("http://localhost:3000/api/contacts/" + id).pipe(map(res => res));
+  }
 }
--- client/src/app/contacts/contacts.component.ts
+++ client/src/app/contacts/contacts.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ContactService } from "../contact.service";
+import { Contact } from "../contact";
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
+  providers: [ContactService]
 })
 export class ContactsComponent implements OnInit {
 
-  constructor() { }
+  contacts?: Contact[];
+  contact?: Contact;
+  firstName?: string;
+  lastName?: string;
+  phone?: string;
+
+  constructor(private contactService : ContactService) { }
 
   ngOnInit(): void {
+    this.contactService.getContacts().subscribe(contacts => this.contacts);
   }
 
 }
